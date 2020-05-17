7f555b97cd7280bc54527b948725028eb248d6df
Hari <mhprasad92@gmail.com>
2020-05-17 10:49:34 -0400

    css fix.

--- src/Main/Menu.js
+++ src/Main/Menu.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import './Main.css';
-import './Menu.css';
+import React from "react";
 
 function Menu(props) {
   return (
     <div className="App-main">
-        <div className="main-content">
-            <ul className="menu-items">
-                <li onClick={() => props.onMenuItemClick('home')}> HOME </li>
-                <li onClick={() => props.onMenuItemClick('aboutus')}> ABOUT US </li>
-                <li onClick={() => props.onMenuItemClick('contactus')}> CONTACT US </li>
-            </ul>
-        </div>
+      <div className="main-content">
+        <ul className="menu-items">
+          <li onClick={() => props.onMenuItemClick("home")}> HOME </li>
+          <li onClick={() => props.onMenuItemClick("aboutus")}> ABOUT US </li>
+          <li onClick={() => props.onMenuItemClick("contactus")}>
+            {" "}
+            CONTACT US{" "}
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
